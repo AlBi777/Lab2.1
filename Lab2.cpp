58211b5a02c964634f1ae0d308cdc73874bdb62b
AlexanderBipert <aleksandrbi777@gmail.com>
2022-11-05 16:36:11 +0700

    Правильный ход

--- Lab2.cpp
+++ Lab2.cpp
@@ -1,36 +1,39 @@
 ﻿
-struct hats
+struct camera
 {
-
-}; 
-struct shoes
-{
-
-};
-struct 
-{
-
-};
-struct MyStruct
-{
-
-};
-struct MyStruct
-{
-
-};
-struct MyStruct
-{
-
+	int main_camera;
+	int front_camera;
 };
 
-int main() {
+struct battery
+{
+	int capacity;
+	char *type;
+};
 
+struct memory
+{
+	int ram;
+	int rom;
+};
 
+struct screen {
+	int diagonal;
+	char* type;
+};
 
+struct system
+{
+	char name;
+	int version;
+};
 
-
-}
-
-
+struct smartphone
+{
+	struct camera camera_get;
+	struct battery battery_get;
+	struct memory memory_get;
+	struct screen screen_get;
+	struct system system_get;
+};
 
